c70e1504f7db8c3be20f99a5c2c2e349d3c30f89
Osea <131997084+OKSCODES@users.noreply.github.com>
2026-06-25 00:47:29 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,11 +8,11 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-        <Toaster position="top-right" toastOptions={{ duration: 3000 }} />
-      </AuthProvider>
-    </BrowserRouter>
+      <BrowserRouter basename="/gfa-attendance-system">
+        <AuthProvider>
+          <App />
+          <Toaster position="top-right" />
+        </AuthProvider>
+      </BrowserRouter>
   </React.StrictMode>,
 )
